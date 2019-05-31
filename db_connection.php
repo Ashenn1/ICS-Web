2fea9a091cc72c52913adc65ee3993b79d7eec39
Ashenn1 <sohasanad@gmail.com>
2019-05-31 13:00:21 +0200

    make it better

--- db_connection.php
+++ db_connection.php
@@ -5,7 +5,7 @@ function OpenCon()
 	 $dbhost = "us-cdbr-iron-east-02.cleardb.net";
 	 $dbuser = "b2fc8d6cca888a";
 	 $dbpass = "0c0acf39";
-	 $db = "incidentsdb";
+	 $db = "heroku_bf623a5b75d34c9";
 	 $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
 	 
 	 return $conn;
