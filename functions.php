88398544b1ee8fc4830b18f28615f26bcd0db96e
Ashenn1 <sohasanad@gmail.com>
2019-06-01 23:41:14 +0200

    added username & email exists functions

---
+++ functions.php
@@ -0,0 +1,53 @@
+<?php
+
+include 'db_connection.php';
+$conn = OpenCon();
+
+
+function username_Exists($username){
+
+	$query = "SELECT Username FROM user WHERE Username = ?";
+
+	if($stmt = $conn->prepare($query)){
+
+		$stmt->bind_param("s",$username);
+		$stmt->execute();
+		$stmt->store_result();
+		$stmt->fetch();
+		if($stmt->num_rows == 1){
+			$stmt->close();
+			return true;
+		}
+		$stmt->close();
+
+	}
+
+	return false;
+
+}
+
+function email_Exists($email){
+
+	$query = "SELECT Email FROM user WHERE Email = ?";
+
+	if($stmt = $conn->prepare($query)){
+
+		$stmt->bind_param("s",$email);
+		$stmt->execute();
+		$stmt->store_result();
+		$stmt->fetch();
+		if($stmt->num_rows == 1){
+			$stmt->close();
+			return true;
+		}
+		$stmt->close();
+
+	}
+
+	return false;
+
+}
+
+
+
+?>
