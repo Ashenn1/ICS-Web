7e192135e9229737dff74482fe8d5e30db29b362
Ashenn1 <sohasanad@gmail.com>
2019-06-03 14:25:45 +0200

    added login.php

---
+++ login.php
@@ -0,0 +1,48 @@
+<?php
+
+include 'functions.php';
+
+$conn = OpenCon();
+
+$response = array();
+
+$inputJSON = file_get_contents('php://input');
+$input = json_decode($inputJSON,TRUE);
+
+//check mandatory parameters are set
+if(isset($input['Email']) && isset($input['Password'])){
+
+	$email = $input['Email'];
+	$password = $input['Password'];
+	$query = "SELECT UserId, Username , Email , Password FROM user WHERE Email=? && Password=?";
+
+	if($stmt = $conn->prepare($query)){
+		$stmt->bind_param("ss" , $email , $password);
+		$stmt->execute();
+		$stmt->store_result();
+		$row = $stmt->fetch();
+		if($stmt->num_rows == 1){
+			$response["status"] = 0;
+			$response["message"] = "Login successful";
+			$response["UserId"]= $row["UserId"];
+			$response["Username"] = $row["Username"];
+		}
+		else{
+				$response["status"] = 1;
+				$response["message"] = "Invalid username and password combination";
+			}
+	}
+	else{
+				$response["status"] = 1;
+				$response["message"] = "Invalid username and password combination";
+		}
+}
+else{
+	$response["status"] = 2;
+	$response["message"] = "Missing mandatory parameters";
+}
+
+//Display JSON response.
+echo json_encode($response);
+
+?>
